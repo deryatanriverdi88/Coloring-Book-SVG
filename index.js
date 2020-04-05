00fc1260d35b64ab4fe5e2753803acb261f3b4ca
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-05 03:08:59 -0400

    adds eventListener to selectColor

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let drawing = document.querySelector('#drawing');
 let selectColor = document.querySelector('#select-color');
 let button = document.querySelector('button');
 let color = selectColor.value
+
+selectColor.addEventListener('change', function(event){
+    color = event.target.value
+})
