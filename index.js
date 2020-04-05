aa906a200c5fdf8d57e21caf9a1ef99fe316edae
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-05 03:07:32 -0400

    sets variables

---
+++ index.js
@@ -0,0 +1,4 @@
+let drawing = document.querySelector('#drawing');
+let selectColor = document.querySelector('#select-color');
+let button = document.querySelector('button');
+let color = selectColor.value
